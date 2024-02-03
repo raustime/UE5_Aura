678ff77a8b672a4933ea608665f869db9f7337ca
unknown <raustime@gmail.com>
2024-02-03 19:16:46 +0200

    syncronisation client - server target select by ability

--- Source/Aura/Private/AuraAssetManager.cpp
+++ Source/Aura/Private/AuraAssetManager.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AuraAssetManager.h"
+#include "AbilitySystemGlobals.h"
 #include "AuraGameplayTags.h"
 
 UAuraAssetManager& UAuraAssetManager::Get()
@@ -14,4 +15,7 @@ void UAuraAssetManager::StartInitialLoading()
 {
     Super::StartInitialLoading();
     FAuraGameplayTags::InitializeNativeGameplayTags();
+
+    // This is required to use Target Data!
+    UAbilitySystemGlobals::Get().InitGlobalData();
 }
