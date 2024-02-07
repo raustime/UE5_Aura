208ab0f3089f2e3dbb95a59a46e374d3903c27b2
raustime <mtsprofit@ukr.net>
2024-02-07 17:28:49 +0200

    enemy progress bar

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -5,7 +5,10 @@
 #include "Character/AuraCharacterBase.h"
 #include "CoreMinimal.h"
 #include "Interaction/EnemyInterface.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraEnemy.generated.h"
+
+class UWidgetComponent;
 
 UCLASS()
 class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
@@ -23,10 +26,20 @@ public:
     /** Combat Interface */
     virtual int32 GetPlayerLevel() override;
     /** end Combat Interface */
+
+    UPROPERTY(BlueprintAssignable)
+    FOnAttributeChangedSignature OnHealthChanged;
+
+    UPROPERTY(BlueprintAssignable)
+    FOnAttributeChangedSignature OnMaxHealthChanged;
+
 protected:
     virtual void BeginPlay() override;
     virtual void InitAbilityActorInfo() override;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
     int32 Level = 1;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    TObjectPtr<UWidgetComponent> HealthBar;
 };
