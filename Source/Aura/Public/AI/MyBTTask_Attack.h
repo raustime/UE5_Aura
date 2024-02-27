7524ccceb995a4dd166cce60ff31f11e8f6e712e
raustime <mtsprofit@ukr.net>
2024-02-27 17:27:44 +0200

    Custom BT task

---
+++ Source/Aura/Public/AI/MyBTTask_Attack.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "MyBTTask_Attack.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UMyBTTask_Attack : public UBTTask_BlueprintBase
+{
+    GENERATED_BODY()
+
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
