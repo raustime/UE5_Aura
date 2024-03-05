cdf444257bc22eb462f6c799c8d4aab3926eb104
raustime <mtsprofit@ukr.net>
2024-03-05 16:07:02 +0200

    Melee atack done

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -154,3 +154,11 @@ void UAuraAbilitySystemLibrary::SetIsCriticalHit(FGameplayEffectContextHandle& E
         AuraEffectContext->SetIsCriticalHit(bInIsCriticalHit);
     }
 }
+
+bool UAuraAbilitySystemLibrary::IsNotFriend(AActor* FirstActor, AActor* SecondActor)
+{
+    const bool bBothArePlayers = FirstActor->ActorHasTag(FName("Player")) && SecondActor->ActorHasTag(FName("Player"));
+    const bool bBothAreEnemies = FirstActor->ActorHasTag(FName("Enemy")) && SecondActor->ActorHasTag(FName("Enemy"));
+    const bool bFriends = bBothArePlayers || bBothAreEnemies;
+    return !bFriends;
+}
