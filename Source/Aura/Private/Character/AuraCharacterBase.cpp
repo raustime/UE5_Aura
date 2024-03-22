efe2f4fe3ecc00454544af5aaeaa14573acef1ce
raustime <mtsprofit@ukr.net>
2024-03-22 16:48:14 +0200

    minion counts chk and ranged atack custom pitch

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -121,6 +121,11 @@ int32 AAuraCharacterBase::GetMinionCount_Implementation()
     return MinionCount;
 }
 
+void AAuraCharacterBase::IncremenetMinionCount_Implementation(int32 Amount)
+{
+    MinionCount += Amount;
+}
+
 void AAuraCharacterBase::InitAbilityActorInfo() {}
 
 void AAuraCharacterBase::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GameplayEffectClass, float level) const
