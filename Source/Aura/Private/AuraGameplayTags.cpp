ca3a6da21a89af87ac3f4b115b415464b2a477ee
raustime <mtsprofit@ukr.net>
2024-04-04 17:15:13 +0300

    GA cooldown

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -129,6 +129,13 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
         UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Abilities.Fire.FireBolt"), FString("FireBolt Ability Tag"));
 
     /*
+     * Cooldown
+     */
+
+    GameplayTags.Cooldown_Fire_FireBolt =
+        UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Cooldown.Fire.FireBolt"), FString("FireBolt Cooldown Tag"));
+
+    /*
      * Combat Sockets
      */
 
