0d8e5bc5e2a99fbaacce73fbe94a825c5393df6c
raustime <mtsprofit@ukr.net>
2024-04-10 17:11:18 +0300

    Listening for event ability

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -98,7 +98,6 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
     if (Data.EvaluatedData.Attribute == GetHealthAttribute())
     {
         SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
-        // UE_LOG(LogTemp, Warning, TEXT("Changed Health on %s, Health: %f"), *Props.TargetAvatarActor->GetName(), GetHealth());
     }
 
     if (Data.EvaluatedData.Attribute == GetManaAttribute())
@@ -136,6 +135,13 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
             const bool bCriticalHit = UAuraAbilitySystemLibrary::IsCriticalHit(Props.EffectContextHandle);
             ShowFloatingText(Props, LocalIncomingDamage, bBlock, bCriticalHit);
         }
+    }
+
+    if (Data.EvaluatedData.Attribute == GetIncomingXPAttribute())
+    {
+        const float LocalIncomingXP = GetIncomingXP();
+        SetIncomingXP(0.f);
+        UE_LOG(LogTemp, Log, TEXT("Incoming XP: %f"), LocalIncomingXP);
     }
 }
 
