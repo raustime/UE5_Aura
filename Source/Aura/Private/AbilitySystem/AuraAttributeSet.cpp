3a9f8350981f616316e6bc4906d65c5464c4571d
raustime <mtsprofit@ukr.net>
2024-04-12 14:40:08 +0300

    XPgain  and XPbar

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -8,6 +8,7 @@
 #include "Net/UnrealNetwork.h"
 #include "AuraGameplayTags.h"
 #include "Interaction/CombatInterface.h"
+#include "Interaction/PlayerInterface.h"
 #include "Kismet/GameplayStatics.h"
 #include "Player/AuraPlayerController.h"
 #include "AbilitySystem/AuraAbilitySystemLibrary.h"
@@ -123,6 +124,7 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
                 {
                     CombatInterface->Die();
                 }
+                SendXPEvent(Props);
             }
             else
             {
@@ -141,7 +143,12 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
     {
         const float LocalIncomingXP = GetIncomingXP();
         SetIncomingXP(0.f);
-        UE_LOG(LogTemp, Log, TEXT("Incoming XP: %f"), LocalIncomingXP);
+
+        // TODO: See if we should level up
+        if (Props.SourceCharacter->Implements<UPlayerInterface>())
+        {
+            IPlayerInterface::Execute_AddToXP(Props.SourceCharacter, LocalIncomingXP);
+        }
     }
 }
 
@@ -191,6 +198,22 @@ void UAuraAttributeSet::ShowFloatingText(const FEffectProperties& Props, float D
         {
             PC->ShowDamageNumber(Damage, Props.TargetCharacter, bBlockedHit, bCriticalHit);
         }
+    }
+}
+
+void UAuraAttributeSet::SendXPEvent(const FEffectProperties& Props)
+{
+    if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetCharacter))
+    {
+        const int32 TargetLevel = CombatInterface->GetPlayerLevel();
+        const ECharacterClass TargetClass = ICombatInterface::Execute_GetCharacterClass(Props.TargetCharacter);
+        const int32 XPReward = UAuraAbilitySystemLibrary::GetXPRewardForClassAndLevel(Props.TargetCharacter, TargetClass, TargetLevel);
+
+        const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+        FGameplayEventData Payload;
+        Payload.EventTag = GameplayTags.Attributes_Meta_IncomingXP;
+        Payload.EventMagnitude = XPReward;
+        UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Props.SourceCharacter, GameplayTags.Attributes_Meta_IncomingXP, Payload);
     }
 }
 
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -4,13 +4,14 @@
 
 #include "Character/AuraCharacterBase.h"
 #include "CoreMinimal.h"
+#include "Interaction/PlayerInterface.h"
 #include "AuraCharacter.generated.h"
 
 class UCameraComponent;
 class USpringArmComponent;
 
 UCLASS()
-class AURA_API AAuraCharacter : public AAuraCharacterBase
+class AURA_API AAuraCharacter : public AAuraCharacterBase, public IPlayerInterface
 {
     GENERATED_BODY()
 
@@ -18,6 +19,10 @@ public:
     AAuraCharacter();
     virtual void PossessedBy(AController* NewController) override;
     virtual void OnRep_PlayerState() override;
+
+    /** Players Interface */
+    virtual void AddToXP_Implementation(int32 InXP) override;
+    /** end Player Interface */
 
     /** Combat Interface */
     virtual int32 GetPlayerLevel() override;
