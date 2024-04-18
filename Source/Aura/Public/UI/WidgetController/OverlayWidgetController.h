2c818f99922042df1d55195726aea99372325070
raustime <mtsprofit@ukr.net>
2024-04-18 16:22:32 +0300

    Showing Level in the HUD

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -30,6 +30,7 @@ struct FUIWidgetRow : public FTableRowBase
     UTexture2D* Image = nullptr;
 };
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChangedSignature, int32, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAttributeChangedSignature, float, NewValue);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMessageWidgetRowSignature, FUIWidgetRow, Row);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAbilityInfoSignature, const FAuraAbilityInfo&, Info);
@@ -63,6 +64,9 @@ public:
 
     UPROPERTY(BlueprintAssignable, Category = "GAS|XP")
     FOnAttributeChangedSignature OnXPPercentChangedDelegate;
+
+    UPROPERTY(BlueprintAssignable, Category = "GAS|Level")
+    FOnPlayerStatChangedSignature OnPlayerLevelChangedDelegate;
 
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
